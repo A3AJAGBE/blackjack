bdcbe27524c29b2a67cbe5df160781053a9e5930
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-01-28 02:44:23 +0000

    Add user start prompt and validation

--- main.py
+++ main.py
@@ -2,6 +2,21 @@
 This is a basic blackjack game application
 """
 
+# Imports
+import random
+
+# Default displays
 print('HOUSE RULES:\nUnlimited deck size.\nThere are no Jokers.\nThe Jack,Queen and King counts as 10\n'
       'Ace can count as either 1 or 11\nCards are not removed from the deck as they are drawn\n'
-      'The computer is the dealer')
+      'The computer is the dealer\n')
+
+# User prompts
+user_start = input('Ready to play a game of blackjack? Type "Yes" or "No"\n').title()
+
+# Validate response
+if user_start == "Yes":
+    print('Let\'s Go\n')
+elif user_start == "No":
+    print('When ever you are ready, start the game.')
+else:
+    print('Invalid Response.')
