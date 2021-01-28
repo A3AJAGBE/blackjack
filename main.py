539971128ef8870ab6c6dadf4d0995b4fa5cf948
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-01-28 02:35:02 +0000

    Add house rules

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 """
 This is a basic blackjack game application
 """
+
+print('HOUSE RULES:\nUnlimited deck size.\nThere are no Jokers.\nThe Jack,Queen and King counts as 10\n'
+      'Ace can count as either 1 or 11\nCards are not removed from the deck as they are drawn\n'
+      'The computer is the dealer')
