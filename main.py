80e654cc9bf557d9de8ac858a981ba2041611ab5
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-01-28 03:00:16 +0000

    Add card dealing process

--- main.py
+++ main.py
@@ -16,6 +16,22 @@ user_start = input('Ready to play a game of blackjack? Type "Yes" or "No"\n').ti
 # Validate response
 if user_start == "Yes":
     print('Let\'s Go\n')
+    # Deck Cards
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+    # Dealing cards
+    user_cards = random.sample(cards, 2)
+    computer_cards = random.sample(cards, 2)
+
+    #testing
+    print(f'Dealer\'s card: {computer_cards}')
+
+    user_score = 0
+    for card in user_cards:
+        user_score += card
+    print(f'Your cards are: {user_cards} and the current score is {user_score}')
+    computer_score = 0
+    print(f'The dealer\'s first card is: {computer_cards[0]}')
 elif user_start == "No":
     print('When ever you are ready, start the game.')
 else:
